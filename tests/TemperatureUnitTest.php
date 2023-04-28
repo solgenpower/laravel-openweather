a477b278b46fd4ddf5d775c713996f9f9e5a78d6
princejohnsantillan <mail@princejohnsantillan.net>
2023-04-28 19:03:47 +0800

    Add TmperatureUnitTest

---
+++ tests/TemperatureUnitTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace SolgenPower\LaravelOpenWeather\Test;
+
+use PHPUnit\Framework\TestCase;
+use SolgenPower\LaravelOpenWeather\Enums\TemperatureUnit;
+
+class TemperatureUnitTest extends TestCase
+{
+    /** @test */
+    public function it_returns_temperature_symbol()
+    {
+        $this->assertEquals('°K', TemperatureUnit::Standard->symbol());
+        $this->assertEquals('°F', TemperatureUnit::Imperial->symbol());
+        $this->assertEquals('°C', TemperatureUnit::Metric->symbol());
+    }
+
+    /** @test */
+    /** @test */
+    public function it_returns_temperature_unit()
+    {
+        $this->assertEquals('Kelvin', TemperatureUnit::Standard->unit());
+        $this->assertEquals('Fahrenheit', TemperatureUnit::Imperial->unit());
+        $this->assertEquals('Celsius', TemperatureUnit::Metric->unit());
+    }
+}
